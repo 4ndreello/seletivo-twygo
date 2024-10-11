342b840b8c880e3dcf467fd34ef3334955939312
4ndreello <andreello@pm.me>
2024-10-10 22:29:04 -0300

    add: modal to insert new videos

--- app/course/insert/page.tsx
+++ app/course/insert/page.tsx
@@ -1,11 +1,35 @@
 "use client";
 
 import { ArrowBackIcon, PlusSquareIcon } from "@chakra-ui/icons";
-import { Box, Input, Text, Textarea } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  Textarea,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
+
+const videos: { title: string; link: string }[] = [
+  {
+    title: "Desenvolvimento humano e especializado do gmg",
+    link: "https://www.youtube.com/watch?v=Usnop2ISIbg",
+  },
+  { title: "GMG", link: "https://www.youtube.com/watch?v=Usnop2ISIbg" },
+];
 
 export default function Page() {
   const router = useRouter();
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <Box p={15}>
@@ -17,14 +41,67 @@ export default function Page() {
         <Textarea placeholder="Description" h={200} resize={"none"} />
 
         <Box>
-          <Input variant="outline" placeholder="Video link" />
-        </Box>
+          <p>{"Title"}</p>
+          <Text>Videos controller</Text>
+          <Box
+            borderRadius={6}
+            border={"1px"}
+            borderColor="var(--chakra-colors-chakra-border-color)"
+          >
+            <Button
+              onClick={() => {
+                onOpen();
+              }}
+              borderRadius={0}
+              w={"100%"}
+            >
+              +
+            </Button>
 
-        <Box>
-          <Text>Image</Text>
-          <input type="file" />
+            <Box maxHeight={"28vh"} overflow={"auto"}>
+              {videos.map((video, index) => {
+                return (
+                  <Box key={index} display="flex" p={2}>
+                    <Box display={"flex"} alignItems={"center"}>
+                      <Text>{video.title}</Text>
+                    </Box>
+
+                    <Box marginLeft="auto">
+                      <Button>X</Button>
+                    </Box>
+                  </Box>
+                );
+              })}
+            </Box>
+          </Box>
         </Box>
       </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{"Adding a video"}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box>
+              <Input variant="outline" placeholder="Title" />
+            </Box>
+
+            <br />
+
+            <Box>
+              <Input variant="outline" placeholder="Youtube link" />
+            </Box>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button variant={"outline"} mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button colorScheme="green">Save</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
 
       <Box
         position="fixed"
